b3cd219926b6f83bfa63bf30c73057022b652f41
spurti-nirwani <spurtinirwani2808@gmail.com>
2026-02-05 12:20:17 +0530

    Save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,6 @@
-export let cart = [{
+export let cart = JSON.parse( localStorage.getItem('cart'));
+if(!cart){
+  cart = [{
   productId:
   'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
   quantity: 2,
@@ -6,7 +8,13 @@ export let cart = [{
   productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
   quantity: 1
 }];
+}
 
+
+
+function saveToStorage(){
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 export function addToCart(productId){
     let matchingItem;
 
@@ -24,6 +32,8 @@ export function addToCart(productId){
           quantity:1
       });
       }
+
+      saveToStorage();
 }
 
 
@@ -38,4 +48,6 @@ export function removeFromCart(productId){
 
 
   cart = newCart;
+
+  saveToStorage();
 }
